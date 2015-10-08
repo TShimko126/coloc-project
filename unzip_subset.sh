d8a56065288ef26697036d5b9909d7bf4c7fd3c7
Tyler Shimko <TShimko126@gmail.com>
2015-10-08 08:37:42 -0700

    Change unzip subset script to correct output folder

--- unzip_subset.sh
+++ unzip_subset.sh
@@ -10,4 +10,4 @@ file=`ls -d1 /users/nteran/data/matrixEqtlSubset/cisOutput/* | sed -n ${SGE_TASK
 
 file2=`ls /users/nteran/data/matrixEqtlSubset/cisOutput/ | sed -n ${SGE_TASK_ID}p`
 
-zcat $file > /srv/scratch/tshimko/analysis/tmp/${file2}.txt
+zcat $file > /srv/scratch/tshimko/analysis/tmp_subset/${file2}.txt
