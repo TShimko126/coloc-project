e6e4d8cbee586793bda3ab9954aa1d883a9b3744
Tyler Shimko <TShimko126@gmail.com>
2015-10-08 08:34:51 -0700

    Add unzip script for subset MatrixEQTL results from Nikki

---
+++ unzip_subset.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -m e
+#$ -M tshimko@stanford.edu
+#$ -cwd
+#$ -t 1-44
+#$ -p -30
+#$ -S /bin/bash
+
+file=`ls -d1 /users/nteran/data/matrixEqtlSubset/cisOutput/* | sed -n ${SGE_TASK_ID}p`
+
+file2=`ls /users/nteran/data/matrixEqtlSubset/cisOutput/ | sed -n ${SGE_TASK_ID}p`
+
+zcat $file > /srv/scratch/tshimko/analysis/tmp/${file2}.txt
