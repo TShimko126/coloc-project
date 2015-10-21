f2d00cce85943db2d89c8acb42a0b4ba7f82fa6c
Tyler Shimko <TShimko126@gmail.com>
2015-10-21 14:52:45 -0700

    Modify to do all comparisons in both directions

--- coloc-subset.sh
+++ coloc-subset.sh
@@ -2,10 +2,10 @@
 #$ -m e
 #$ -M tshimko@stanford.edu
 #$ -cwd
-#$ -t 1-946
+#$ -t 1-1892
 #$ -p -50
 #$ -S /bin/bash
 
-files=`sed -n ${SGE_TASK_ID}p tissue_combinations.txt`
+files=`sed -n ${SGE_TASK_ID}p tissue_combinations_full.txt`
 
 Rscript --vanilla ./coloc-analysis/coloc-subset.R $files
